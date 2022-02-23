ceff6befd650162d5a2014df89164748721ee506
Trịnh Đức Duy <Duya3fithou@gmail.com>
2022-02-23 23:47:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const list = document.getElementsByTagName('TR');
 const data = [];
 for (let i = 5; i < list.length; i++) {
-    const quantityCredit = Number(list[i].getElementsByTagName('TD')[1].innerText);
+    const quantity = Number(list[i].getElementsByTagName('TD')[1].innerText);
     const diligence = Number(list[i].getElementsByTagName('TD')[4].innerText);
-    const midtermPoint = Number(list[i].getElementsByTagName('TD')[5].innerText);
-    const endtermPoint = Number(list[i].getElementsByTagName('TD')[6].innerText);
+    const midterm = Number(list[i].getElementsByTagName('TD')[5].innerText);
+    const endterm = Number(list[i].getElementsByTagName('TD')[6].innerText);
     const subjectName = list[i].getElementsByTagName('TD')[0].innerText;
     const subjectId = list[i].getElementsByTagName('TD')[2].innerText;
     const teacher = list[i].getElementsByTagName('TD')[3].innerText;
-    const avgPoint = diligence * 0.1 + midtermPoint * 0.2 + endtermPoint * 0.7;
+    const point_avg = diligence * 0.1 + midterm * 0.2 + endterm * 0.7;
     let new4 = 0;
-    if (avgPoint < 4) new4 = 0;
-    if (avgPoint >= 4 && avgPoint < 5) new4 = 1;
-    if (avgPoint >= 5 && avgPoint < 5.5) new4 = 1.5;
-    if (avgPoint >= 5.5 && avgPoint < 6.5) new4 = 2;
-    if (avgPoint >= 6.5 && avgPoint < 7) new4 = 2.5;
-    if (avgPoint >= 7 && avgPoint < 8) new4 = 3;
-    if (avgPoint >= 8 && avgPoint < 8.5) new4 = 3.5;
-    if (avgPoint >= 8.5) new4 = 4;
-    const coefficientPoint = quantityCredit * new4;
+    if (point_avg < 4) new4 = 0;
+    if (point_avg >= 4 && point_avg < 5) new4 = 1;
+    if (point_avg >= 5 && point_avg < 5.5) new4 = 1.5;
+    if (point_avg >= 5.5 && point_avg < 6.5) new4 = 2;
+    if (point_avg >= 6.5 && point_avg < 7) new4 = 2.5;
+    if (point_avg >= 7 && point_avg < 8) new4 = 3;
+    if (point_avg >= 8 && point_avg < 8.5) new4 = 3.5;
+    if (point_avg >= 8.5) new4 = 4;
+    const diemHeSo = quantity * new4;
     const dataSubject = {
         subjectName,
-        quantityCredit,
+        quantity,
         subjectId,
         teacher,
         diligence,
-        midtermPoint,
-        endtermPoint,
-        avgPoint,
+        midterm,
+        endterm,
+        point_avg,
         new4,
-        coefficientPoint,
+        diemHeSo,
     };
     data.push(dataSubject);
 }
@@ -38,12 +38,12 @@ const dataFilter = data.filter((item, index) => {
     if (
         item.quantity !== 0 &&
         item.subjectName !== data?.[index + 1]?.subjectName &&
-        item.avgPoint >= 4
+        item.point_avg >= 4
     ) {
         return item;
     }
 });
-console.log('Danh sách đã qua môn: ', dataFilter);
+console.log('Danh sách đã qua môn:', dataFilter);
 let totalQuantity = 0;
 let tongDiemHeSo = 0;
 dataFilter.forEach(item => {
